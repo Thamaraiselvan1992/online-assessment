0133c9f3c7ff8735627fda2d18cc7a77874eb024
selvan1992 <selvan@erisn.com>
2022-04-13 17:22:43 +0530

    req 29

--- onlineTest/urls.py
+++ onlineTest/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls,name='admin'),
     path("", include("apps.authentication.urls")), # Auth routes - login / register
     path("", include("apps.home.urls"))  
 ]
