8f1be7e8a97254298d2b90c1288e242665dddb7f
selvan1992 <selvan@erisn.com>
2022-04-13 12:16:57 +0530

    req 2

--- onlineTest/settings.py
+++ onlineTest/settings.py
@@ -163,7 +163,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(CORE_DIR, '/apps/static'),
+    os.path.join(CORE_DIR, 'apps/static'),
 )
 
 MEDIA_URL = 'media/'
